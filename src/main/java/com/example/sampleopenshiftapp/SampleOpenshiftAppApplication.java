c2d85d4d92bb521c77e912cb7641a6d2d9dda811
vfichtner <v.fichtner@gmail.com>
2020-02-12 22:02:56 +0100

    Initial commit

---
+++ src/main/java/com/example/sampleopenshiftapp/SampleOpenshiftAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleopenshiftapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleOpenshiftAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleOpenshiftAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sampleopenshiftapp/SampleOpenshiftAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleopenshiftapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleOpenshiftAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
